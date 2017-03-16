698d934c2782e1b0394369a0b6b63c62628b3400
Roberto Arista <arista.rob@gmail.com>
2017-03-16 11:59:34 +0100

    the interface for the stem/diagonal section is on. Functionality will follow soon Some minor adjustment with colors and ctrls heights handling

--- Drawing Assistant.py
+++ Drawing Assistant.py
@@ -20,7 +20,7 @@ from math import cos, sin, radians
 from lib.eventTools.eventManager import getActiveEventTool
 from mojo.UI import UpdateCurrentGlyphView, AccordionView
 from vanilla import FloatingWindow, CheckBox, HorizontalLine, PopUpButton, Group
-from vanilla import TextBox, EditText, ColorWell
+from vanilla import TextBox, EditText, ColorWell, SquareButton
 from mojo.drawingTools import *
 from defconAppKit.windows.baseWindow import BaseWindowController
 from AppKit import NSFont, NSColor, NSFontAttributeName
@@ -33,13 +33,18 @@ GRID_TOLERANCE = 2
 
 # ui
 BODYSIZE_CAPTION = 10
+
 BLACK_COLOR = NSColor.blackColor()
-COLOR_ONE_INIT = (0.88, 1, 0.4, 1)
-COLOR_TWO_INIT = (0.4, 1, 0.64, 1)
-COLOR_THREE_INIT = (0.4, 0.64, 1, 1)
-COLOR_FOUR_INIT = (0.88, 0.4, 1, 1)
-COLOR_FIVE_INIT = (1, 0.4, 0.4, 1)
-INIT_COLORS = [COLOR_ONE_INIT, COLOR_TWO_INIT, COLOR_THREE_INIT, COLOR_FOUR_INIT, COLOR_FIVE_INIT]
+
+STEM_COLOR = (1, 127/255., 102/255., 1)
+DIAGONAL_COLOR = (102/255., 124/255., 1, 1)
+
+GRID_COLOR_ONE = (0.88, 1, 0.4, 1)
+GRID_COLOR_TWO = (0.4, 1, 0.64, 1)
+GRID_COLOR_THREE = (0.4, 0.64, 1, 1)
+GRID_COLOR_FOUR = (0.88, 0.4, 1, 1)
+GRID_COLOR_FIVE = (1, 0.4, 0.4, 1)
+GRID_COLOR_INIT = [GRID_COLOR_ONE, GRID_COLOR_TWO, GRID_COLOR_THREE, GRID_COLOR_FOUR, GRID_COLOR_FIVE]
 
 PLUGIN_WIDTH = 220
 
@@ -71,28 +76,29 @@ class MultipleGridController(Group):
         Group.__init__(self, posSize)
         assert activeCtrls <= ctrlsAmount
 
+        self.ctrlHeight = posSize[3]
         self.gridActive = gridActive
         self.ctrlsAmount = ctrlsAmount
         self.activeCtrls = activeCtrls
         self.gridIndexes = ['%d' % integ for integ in range(1, ctrlsAmount+1)]
         self.callback = callback
 
-        self.gridsDB = [{'horizontal': False, 'vertical': False, 'step': None, 'color': color} for color in INIT_COLORS]
+        self.gridsDB = [{'horizontal': False, 'vertical': False, 'step': None, 'color': color} for color in GRID_COLOR_INIT]
 
-        jumpingY = 4
-        self.gridActiveCheck = CheckBox((0, jumpingY, NET_WIDTH*.6, vanillaControlsSize['CheckBoxRegularHeight']),
+        jumpin_Y = 4
+        self.gridActiveCheck = CheckBox((0, jumpin_Y, NET_WIDTH*.6, vanillaControlsSize['CheckBoxRegularHeight']),
                                         "Show grids",
                                         value=self.gridActive,
                                         callback=self.gridActiveCheckCallback)
 
         for eachI in range(1, ctrlsAmount+1):
-            jumpingY += vanillaControlsSize['EditTextRegularHeight'] + MARGIN_VER
-            gridCtrl = SingleGridController((0, jumpingY, NET_WIDTH, vanillaControlsSize['EditTextRegularHeight']),
+            jumpin_Y += vanillaControlsSize['EditTextRegularHeight'] + MARGIN_VER
+            gridCtrl = SingleGridController((0, jumpin_Y, NET_WIDTH, vanillaControlsSize['EditTextRegularHeight']),
                                             index=eachI,
                                             isVertical=False,
                                             isHorizontal=False,
                                             step=None,
-                                            gridColor=INIT_COLORS[eachI-1],
+                                            gridColor=GRID_COLOR_INIT[eachI-1],
                                             callback=self.gridCtrlCallback)
             gridCtrl.enable(self.gridActive)
             setattr(self, 'grid%#02d' % eachI, gridCtrl)
@@ -130,27 +136,27 @@ class SingleGridController(Group):
         self.callback = callback
 
         # ctrls
-        jumpinX = 12
-        self.indexText = TextBox((jumpinX, 0, 16, vanillaControlsSize['TextBoxRegularHeight']), '%d)' % index)
-        jumpinX += self.indexText.getPosSize()[2]
+        jumpin_X = 12
+        self.indexText = TextBox((jumpin_X, 0, 16, vanillaControlsSize['TextBoxRegularHeight']), '%d)' % index)
+        jumpin_X += self.indexText.getPosSize()[2]
 
-        self.stepCtrl = EditText((jumpinX, 0, 24, vanillaControlsSize['EditTextRegularHeight']),
+        self.stepCtrl = EditText((jumpin_X, 0, 24, vanillaControlsSize['EditTextRegularHeight']),
                                  callback=self.stepCtrlCallback)
-        jumpinX += self.stepCtrl.getPosSize()[2] + 16
+        jumpin_X += self.stepCtrl.getPosSize()[2] + 16
 
-        self.isHorizontalCheck = CheckBox((jumpinX, 0, 32, vanillaControlsSize['CheckBoxRegularHeight']),
+        self.isHorizontalCheck = CheckBox((jumpin_X, 0, 32, vanillaControlsSize['CheckBoxRegularHeight']),
                                           "H",
                                           value=self.isHorizontal,
                                           callback=self.isHorizontalCheckCallback)
-        jumpinX += self.isHorizontalCheck.getPosSize()[2]+2
+        jumpin_X += self.isHorizontalCheck.getPosSize()[2]+2
 
-        self.isVerticalCheck = CheckBox((jumpinX, 0, 32, vanillaControlsSize['CheckBoxRegularHeight']),
+        self.isVerticalCheck = CheckBox((jumpin_X, 0, 32, vanillaControlsSize['CheckBoxRegularHeight']),
                                         "V",
                                         value=self.isVertical,
                                         callback=self.isVerticalCheckCallback)
-        jumpinX += self.isVerticalCheck.getPosSize()[2]+10
+        jumpin_X += self.isVerticalCheck.getPosSize()[2]+10
 
-        self.whichColorWell = ColorWell((jumpinX, 0, 46, self.ctrlHeight),
+        self.whichColorWell = ColorWell((jumpin_X, 0, 46, self.ctrlHeight),
                                         color=NSColor.colorWithCalibratedRed_green_blue_alpha_(*gridColor),
                                         callback=self.whichColorWellCallback)
 
@@ -188,6 +194,91 @@ class SingleGridController(Group):
         self.callback(self)
 
 
+class DistancesController(Group):
+
+    def __init__(self, posSize, stemActive, diagonalActive, callback):
+        Group.__init__(self, posSize)
+        self.callback = callback
+        self.ctrlWidth, self.ctrlHeight = posSize[2], posSize[3]
+        self.stemActive = stemActive
+        self.diagonalActive = diagonalActive
+
+        self.stemColor = STEM_COLOR
+        self.diagonalColor = DIAGONAL_COLOR
+
+        jumpin_Y = 4
+        self.stemsCheck = CheckBox((0, jumpin_Y, self.ctrlWidth*.6, vanillaControlsSize['CheckBoxRegularHeight']),
+                                   "Show stems",
+                                   value=self.stemActive,
+                                   callback=self.stemsCheckCallback)
+
+        self.stemsColorWell = ColorWell((-MARGIN_HOR-46, jumpin_Y, 46, vanillaControlsSize['CheckBoxRegularHeight']),
+                                        color=NSColor.colorWithCalibratedRed_green_blue_alpha_(*self.stemColor),
+                                        callback=self.stemsColorWellCallback)
+
+        jumpin_Y += vanillaControlsSize['CheckBoxRegularHeight'] + MARGIN_VER*.5
+        self.distancesCheck = CheckBox((0, jumpin_Y, self.ctrlWidth*.6, vanillaControlsSize['CheckBoxRegularHeight']),
+                                       "Show diagonals",
+                                       value=self.diagonalActive,
+                                       callback=self.distancesCheckCallback)
+
+        self.distancesColorWell = ColorWell((-MARGIN_HOR-46, jumpin_Y, 46, vanillaControlsSize['CheckBoxRegularHeight']),
+                                            color=NSColor.colorWithCalibratedRed_green_blue_alpha_(*self.diagonalColor),
+                                            callback=self.distancesColorWellCallback)
+
+        jumpin_Y += vanillaControlsSize['CheckBoxRegularHeight'] + MARGIN_VER
+        self.addStemButton = SquareButton((0, jumpin_Y, self.ctrlWidth*.45, vanillaControlsSize['ButtonRegularHeight']*2),
+                                          "Add\nStem",
+                                          callback=self.addStemButtonCallback)
+
+        self.addDistanceButton = SquareButton((-self.ctrlWidth*.45-MARGIN_HOR, jumpin_Y, self.ctrlWidth*.45, vanillaControlsSize['ButtonRegularHeight']*2),
+                                              "Add\nDiagonal",
+                                              callback=self.addDistanceButtonCallback)
+
+        jumpin_Y += vanillaControlsSize['ButtonRegularHeight']*2+MARGIN_VER
+        self.deleteButton = SquareButton((0, jumpin_Y, -MARGIN_HOR, vanillaControlsSize['ButtonRegularHeight']*1.5),
+                                         "Delete",
+                                         callback=self.deleteButtonCallback)
+
+
+    def stemsCheckCallback(self, sender):
+        print sender.get()
+        self.callback(self)
+
+    def stemsColorWellCallback(self, sender):
+        calibratedColor = sender.get()
+        self.stemColor = (calibratedColor.redComponent(),
+                          calibratedColor.greenComponent(),
+                          calibratedColor.blueComponent(),
+                          calibratedColor.alphaComponent())
+        self.callback(self)
+
+    def distancesCheckCallback(self, sender):
+        calibratedColor = sender.get()
+        self.diagonalColor = (calibratedColor.redComponent(),
+                              calibratedColor.greenComponent(),
+                              calibratedColor.blueComponent(),
+                              calibratedColor.alphaComponent())
+        self.callback(self)
+
+    def distancesColorWellCallback(self, sender):
+        print sender.get()
+        self.callback(self)
+
+    def addStemButtonCallback(self, sender):
+        print 'addStemButtonCallback button!'
+        self.callback(self)
+
+    def addDistanceButtonCallback(self, sender):
+        print 'addStemButtonCallback button!'
+        self.callback(self)
+
+    def deleteButtonCallback(self, sender):
+        print 'addStemButtonCallback button!'
+        self.callback(self)
+
+
+
 class DrawingAssistant(BaseWindowController):
     """the aim of this plugin is to provide useful visualizations on glyph canvas"""
 
@@ -195,13 +286,15 @@ class DrawingAssistant(BaseWindowController):
     sqrActive = False
     bcpLengthActive = False
     gridActive = False
+    stemActive = False
+    diagonalActive = False
 
     gridsDB = []
 
     def __init__(self):
 
         # init views
-        self.bcpController = BcpController((MARGIN_HOR, 0, NET_WIDTH, vanillaControlsSize['CheckBoxRegularHeight']*2),
+        self.bcpController = BcpController((MARGIN_HOR, 0, NET_WIDTH, vanillaControlsSize['CheckBoxRegularHeight']*2.5),
                                            sqrActive=self.sqrActive,
                                            bcpLengthActive=self.bcpLengthActive,
                                            callback=self.bcpController)
@@ -212,19 +305,21 @@ class DrawingAssistant(BaseWindowController):
                                                      activeCtrls=0,
                                                      callback=self.gridControllerCallback)
 
+        self.distancesController = DistancesController((MARGIN_HOR, 0, NET_WIDTH, 148),
+                                                       stemActive=self.stemActive,
+                                                       diagonalActive=self.diagonalActive,
+                                                       callback=self.distancesControllerCallback)
+
         # collect views
         accordionItems = [
-                {'label': 'bcp ctrl', 'view': self.bcpController, 'size': vanillaControlsSize['CheckBoxRegularHeight']*2.5, 'collapsed': False, 'canResize': False},
-                {'label': 'grid ctrl', 'view': self.gridController, 'size': 190, 'collapsed': False, 'canResize': False},
+                {'label': 'bcp controller',       'view': self.bcpController,       'size': self.bcpController.ctrlHeight,       'collapsed': False, 'canResize': False},
+                {'label': 'grid controller',      'view': self.gridController,      'size': self.gridController.ctrlHeight,      'collapsed': False, 'canResize': False},
+                {'label': 'distances controller', 'view': self.distancesController, 'size': self.distancesController.ctrlHeight, 'collapsed': False, 'canResize': False},
             ]
 
         # init window with accordion obj
         self.w = FloatingWindow((0, 0, PLUGIN_WIDTH, 600), PLUGIN_KEY)
         self.w.accordionView = AccordionView((0, 0, -0, -0), accordionItems)
-
-        # stems
-
-        # neighbour
 
         # add observers
         addObserver(self, "_draw", "draw")
@@ -424,24 +519,29 @@ class DrawingAssistant(BaseWindowController):
         self.gridActive, self.gridsDB = sender.get()
         UpdateCurrentGlyphView()
 
+    def distancesControllerCallback(self, sender):
+        print sender
+        UpdateCurrentGlyphView()
+
 
 class BcpController(Group):
 
     def __init__(self, posSize, sqrActive, bcpLengthActive, callback):
         Group.__init__(self, posSize)
 
+        self.ctrlHeight = posSize[3]
         self.sqrActive = sqrActive
         self.bcpLengthActive = bcpLengthActive
         self.callback = callback
 
-        jumpingY = 2
-        self.sqrCheck = CheckBox((0, jumpingY, NET_WIDTH, vanillaControlsSize['CheckBoxRegularHeight']),
+        jumpin_Y = 2
+        self.sqrCheck = CheckBox((0, jumpin_Y, NET_WIDTH, vanillaControlsSize['CheckBoxRegularHeight']),
                                  "Show squarings",
                                  value=self.sqrActive,
                                  callback=self.sqrCheckCallback)
 
-        jumpingY += vanillaControlsSize['CheckBoxRegularHeight']
-        self.bcpLengthCheck = CheckBox((0, jumpingY, NET_WIDTH, vanillaControlsSize['CheckBoxRegularHeight']),
+        jumpin_Y += vanillaControlsSize['CheckBoxRegularHeight']
+        self.bcpLengthCheck = CheckBox((0, jumpin_Y, NET_WIDTH, vanillaControlsSize['CheckBoxRegularHeight']),
                                        "Show bcp length",
                                        value=self.bcpLengthActive,
                                        callback=self.bcpLengthCheckCallback)
