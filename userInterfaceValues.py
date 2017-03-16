5a252c856b8ae7a2990d9c340b9db34753f96b83
Roberto Arista <arista.rob@gmail.com>
2017-03-16 10:04:16 +0100

    here I collect some values which I use often along different plugins, especially vanilla controls

---
+++ userInterfaceValues.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Here some values I use constantly across different plugins"""
+
+vanillaControlsSize = {
+    'HorizontalLineThickness': 1,
+    'VerticalLineThickness': 1,
+
+    'ButtonRegularHeight': 20,
+    'ButtonSmallHeight': 17,
+    'ButtonMiniHeight': 14,
+
+    'TextBoxRegularHeight': 17,
+    'TextBoxSmallHeight': 14,
+    'TextBoxMiniHeight': 12,
+
+    'EditTextRegularHeight': 22,
+    'EditTextSmallHeight': 19,
+    'EditTextMiniHeight': 16,
+
+    'CheckBoxRegularHeight': 22,
+    'CheckBoxSmallHeight': 18,
+    'CheckBoxMiniHeight': 10,
+
+    'ComboBoxRegularHeight': 21,
+    'ComboBoxSmallHeight': 17,
+    'ComboBoxMiniHeight': 14,
+
+    'PopUpButtonRegularHeight': 20,
+    'PopUpButtonSmallHeight': 17,
+    'PopUpButtonMiniHeight': 15,
+
+    'SliderWithoutTicksRegularHeight': 15,
+    'SliderWithoutTicksSmallHeight': 11,
+    'SliderWithoutTicksMiniHeight': 10,
+
+    'SliderWithTicksRegularHeight': 23,
+    'SliderWithTicksSmallHeight': 17,
+    'SliderWithTicksMiniHeight': 16,
+    }
