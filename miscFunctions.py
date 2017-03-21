4990d52e2af264e65b0fd6a7ccbee435f2a09bd8
Roberto Arista <arista.rob@gmail.com>
2017-03-21 15:56:00 +0100

    here I will collect some misc function until a better grouping does not emerge. Right now I’ve put here some functions to deal with stems visualization and point ID management

---
+++ miscFunctions.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""handy functions for dealing with a bit of anything"""
+
+import calcFunctions
+reload(calcFunctions)
+from calcFunctions import isBlackInBetween, calcDistanceBetweenPTs
+
+def collectIDsFromSelectedPoints(glyph):
+    selectedIDs = []
+    for eachContour in glyph:
+        for eachSegment in eachContour:
+            if eachSegment.onCurve.selected is True:
+                selectedIDs.append(eachSegment.onCurve.naked().uniqueID)
+    selectedIDs.sort()
+    return selectedIDs
+
+
+def getPointFromID(glyph, ID):
+    for eachContour in glyph:
+        for eachPt in eachContour:
+            if eachPt.onCurve.naked().uniqueID == ID:
+                return eachPt.onCurve
+    return None
+
+
+def guessStemPoints(glyph):
+    guessedStems = []
+    selectedIDs = collectIDsFromSelectedPoints(glyph)
+    remainingIDs = list(selectedIDs)
+    for eachSelectedID in selectedIDs:
+        if eachSelectedID in remainingIDs:
+            eachSelectedPT = getPointFromID(glyph, eachSelectedID)
+            remainingIDs.remove(eachSelectedID)
+
+            possibleCombinations = []
+            for eachRemainingID in remainingIDs:
+                eachRemainingPT = getPointFromID(glyph, eachRemainingID)
+                if isBlackInBetween(glyph, eachSelectedPT, eachRemainingPT) is True:
+                    distance = calcDistanceBetweenPTs(eachSelectedPT, eachRemainingPT)
+                    possibleCombinations.append((distance, eachRemainingID))
+
+            possibleCombinations = sorted(possibleCombinations, key=lambda item:item[0])
+            nearestID = possibleCombinations[0][1]
+            remainingIDs.remove(nearestID)
+
+            stem = [eachSelectedID, nearestID]
+            stem.sort()
+            guessedStems.append(tuple(stem))
+
+    return guessedStems
+
+
+
