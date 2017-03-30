52acd085920cef47613c0af4d679ce350526d1e2
Roberto Arista <arista.rob@gmail.com>
2017-03-30 10:36:54 +0200

    Revert "minor fixes"
    This reverts commit 8c10e52ee51ffebb2a8a44b6d971896b250f091b.

--- ttools/kerningController.py
+++ ttools/kerningController.py
@@ -1,21 +1,23 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-################################
-# TT mighty kerning controller #
-################################
+######################
+# Python Boilerplate #
+######################
 
 ### Modules
 from vanilla import Window
 from defconAppKit.windows.baseWindow import BaseWindowController
 
+
 ### Constants
+
 
 ### Controllers
 class KerningController(BaseWindowController):
     """docstring for KerningController"""
     def __init__(self):
-        super(BaseWindowController, self).__init__()
+        super(KerningController, self).__init__()
 
         self.w = Window((0,0,600,800))
 
