34aa0f9ea2de524441e0746a6e7c429a4b120dfa
Roberto Arista <arista.rob@gmail.com>
2017-05-10 22:25:52 +0200

    added the colors we use in Robofont UI

--- ttools/userInterfaceValues.py
+++ ttools/userInterfaceValues.py
@@ -3,6 +3,26 @@
 
 """Here some values I use constantly across different plugins"""
 
+from collections import OrderedDict
+
+glyphCollectionColors = OrderedDict([('Light Violet', (0.84709999999999996, 0.84709999999999996, 1, 1)),
+                                     ('Medium Violet', (0.74509999999999998, 0.74509999999999998, 1, 1)),
+                                     ('Violet', (0.58819999999999995, 0.58819999999999995, 1, 1)),
+
+                                     ('Light Cyan', (0.84309999999999996, 1, 1, 1)),
+                                     ('Medium Cyan', (0.67449999999999999, 1, 1, 1)),
+                                     ('Cyan', (0.502, 1, 1, 1)),
+
+                                     ('Light Pink', (1, 0.84309999999999996, 0.84309999999999996, 1)),
+                                     ('Medium Pink', (1, 0.64710000000000001, 0.64710000000000001, 1)),
+                                     ('Pink', (1, 0.54120000000000001, 0.54120000000000001, 1)),
+
+                                     ('Light Yellow', (1, 1, 0.84709999999999996, 1)),
+                                     ('Medium Yellow', (1, 1, 0.68240000000000001, 1)),
+                                     ('Yellow', (1, 1, 0.502, 1)),
+                                     ])
+
+
 vanillaControlsSize = {
     'HorizontalLineThickness': 1,
     'VerticalLineThickness': 1,
