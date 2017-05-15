f34ff5aee5326514a598be2be1d9da2a173575e8
Roberto Arista <arista.rob@gmail.com>
2017-05-15 14:26:05 +0200

    new tools for aglfn issues

---
+++ sandbox/isAGLFNcomplaint.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#################################
+# Check if every glyph is AGLFN #
+#################################
+
+### Modules
+import os
+from loadAGLFN import loadAGLFN
+
+### Constants
+
+### Function & procedures
+
+### Variables
+sourceFont = CurrentFont()
+
+### Instructions
+AGLFN = loadAGLFN()
+for eachGlyph in sourceFont:
+    if eachGlyph.unicode:
+        uniHex = ('%X' % eachGlyph.unicode).zfill(4)
+        if uniHex in AGLFN:
+            if AGLFN[uniHex]['glyphName'] != eachGlyph.name:
+                print
+                print sourceFont.info.familyName, sourceFont.info.styleName
+                print uniHex
+                print 'AGLFN name: %s' % AGLFN[uniHex]['glyphName']
+                print 'actual name: %s' % eachGlyph.name
+                print '-'*30
+        else:
+            print '%s not in aglfn, you should change name in uni%s' % (eachGlyph.name, uniHex)
+
+    else:
+        print '%s glyph does not have any unicode value, i cannot tell, sorry' % eachGlyph.name
