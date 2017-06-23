5ed5d5849a745f75262ad980fae3f525831bea2a
Roberto Arista <arista.rob@gmail.com>
2017-06-23 12:22:35 +0200

    here i am going to collect some rules in order to inflate kerning quickly

---
+++ sandbox/rulesToExpandKerning.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+1) f followed by superior and numerator +60
+2) UC2LCAcc are 40% less than UC2LC corrections
+3) capital interpunction follow basic interpunction
+"""
