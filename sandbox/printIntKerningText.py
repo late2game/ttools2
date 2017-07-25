6fb92bad47146dac4e7ace0ded45a39c18e8f768
Roberto Arista <arista.rob@gmail.com>
2017-07-25 16:20:03 +0200

    now printing figures vs interpunction

--- sandbox/printIntKerningText.py
+++ sandbox/printIntKerningText.py
@@ -15,8 +15,9 @@ from string import uppercase, lowercase
 ### Variables
 interpunction = [u'(', u')', u'[', u']', u'{', u'}', u'/', u'\\', u'‹', u'›', u'-', u'‘', u'’', u'\'', u'.', u'•', u'·', u'*', u'°']
 lcLetters = ['f', 'k', 'r', 't', 'v', 'w', 'x', 'y', 'z', u'&', u'ß']
+myDigits = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
 theString = u"%(sep)s(%(gn)s)%(sep)s)%(gn)s(%(sep)s[%(gn)s]%(sep)s\n%(sep)s]%(gn)s[%(sep)s{%(gn)s}%(sep)s}%(gn)s{%(sep)s\n%(sep)s\%(gn)s//%(sep)s//%(gn)s\%(sep)s\n%(sep)s‹%(gn)s›%(sep)s›%(gn)s‹%(sep)s-%(gn)s-%(sep)s\n%(sep)s“%(gn)s”%(sep)s\"%(gn)s\"%(sep)s\n%(sep)s.%(gn)s.%(sep)s•%(gn)s•%(sep)s·%(gn)s·%(sep)s\n%(sep)s*%(gn)s*%(sep)s°%(gn)s°%(sep)s"
 
 ### Instructions
-for eachLetter in interpunction:
-    print theString % {'gn': eachLetter, 'sep': 'll'}
+for eachLetter in myDigits:
+    print theString % {'gn': '/%s ' % eachLetter, 'sep': 'll'}
