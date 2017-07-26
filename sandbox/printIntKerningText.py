66e949d80d09d6c2d6213fcdf98cad4d31e0974c
Roberto Arista <arista.rob@gmail.com>
2017-07-26 10:27:21 +0200

    semicolon class added to kerning texts

--- sandbox/printIntKerningText.py
+++ sandbox/printIntKerningText.py
@@ -13,11 +13,12 @@ from string import uppercase, lowercase
 ### Functions & Procedures
 
 ### Variables
-interpunction = [u'(', u')', u'[', u']', u'{', u'}', u'/', u'\\', u'‹', u'›', u'-', u'‘', u'’', u'\'', u'.', u'•', u'·', u'*', u'°']
+interpunction = [u'(', u')', u'[', u']', u'{', u'}', u'/', u'\\', u'‹', u'›', u'-', u'‘', u'’', u'\'', u'.', u':', u'·', u'•', u'*', u'°']
 lcLetters = ['f', 'k', 'r', 't', 'v', 'w', 'x', 'y', 'z', u'&', u'ß']
 myDigits = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
-theString = u"%(sep)s(%(gn)s)%(sep)s)%(gn)s(%(sep)s[%(gn)s]%(sep)s\n%(sep)s]%(gn)s[%(sep)s{%(gn)s}%(sep)s}%(gn)s{%(sep)s\n%(sep)s\%(gn)s//%(sep)s//%(gn)s\%(sep)s\n%(sep)s‹%(gn)s›%(sep)s›%(gn)s‹%(sep)s-%(gn)s-%(sep)s\n%(sep)s“%(gn)s”%(sep)s\"%(gn)s\"%(sep)s\n%(sep)s.%(gn)s.%(sep)s•%(gn)s•%(sep)s·%(gn)s·%(sep)s\n%(sep)s*%(gn)s*%(sep)s°%(gn)s°%(sep)s"
+myCaps = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'J', 'K', 'L', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+theString = u"%(sep)s(%(gn)s)%(sep)s)%(gn)s(%(sep)s[%(gn)s]%(sep)s\n%(sep)s]%(gn)s[%(sep)s{%(gn)s}%(sep)s}%(gn)s{%(sep)s\n%(sep)s\%(gn)s//%(sep)s//%(gn)s\%(sep)s\n%(sep)s‹%(gn)s›%(sep)s›%(gn)s‹%(sep)s-%(gn)s-%(sep)s\n%(sep)s“%(gn)s”%(sep)s\"%(gn)s\"%(sep)s\n%(sep)s.%(gn)s.%(sep)s:%(gn)s:%(sep)s·%(gn)s·%(sep)s\n%(sep)s•%(gn)s•%(sep)s*%(gn)s*%(sep)s°%(gn)s°%(sep)s"
 
 ### Instructions
-for eachLetter in myDigits:
-    print theString % {'gn': '/%s ' % eachLetter, 'sep': 'll'}
+for eachLetter in interpunction:
+    print theString % {'gn': '%s' % eachLetter, 'sep': 'll'}
