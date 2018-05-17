25d587bc7b56a0bf1898e0b8e6b19116ba980bef
Roberto Arista <arista.rob@gmail.com>
2018-05-17 11:39:33 +0200

    added germandbls, daggers, question exclam up and down

--- scripts/glyph names management/makeUC2INT_kerningText.py
+++ scripts/glyph names management/makeUC2INT_kerningText.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-letters = ['O', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'J', 'K', 'L', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'Æ', '&']
+letters = ['O', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'J', 'K', 'L', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'Æ', '&', 'ß']
 template = '''II({letter})II){letter}(II[{letter}]II
 II]{letter}[II{{{letter}}}II}}{letter}{{II
 II/parenleft.case {letter}/parenright.case II/parenright.case {letter}/parenleft.case II/bracketleft.case {letter}/bracketright.case II
@@ -10,8 +10,13 @@ II\\{letter}//II//{letter}\\II
 II‹{letter}›II›{letter}‹II-{letter}-II
 II/guilsinglleft.case {letter}/guilsinglright.case II/guilsinglright.case {letter}/guilsinglleft.case II/hyphen.case {letter}/hyphen.case II
 II“{letter}”II"{letter}"II
+II{letter}/dagger II{letter}/daggerdbl II
+II{letter}/ordfeminine II{letter}/ordmasculine II
 II.{letter}.II:{letter}:II·{letter}·II
-II•{letter}•II*{letter}*II°{letter}°II'''
+II•{letter}•II*{letter}*II°{letter}°II
+II?{letter}?II!{letter}!II
+II¿{letter}¿II¡{letter}¡II
+II/questiondown.case {letter}/questiondown.case II/exclamdown.case {letter}/exclamdown.case II'''
 
 if __name__ == '__main__':
     for eachLetter in letters:
