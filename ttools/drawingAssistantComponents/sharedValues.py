d2ed20675590d5a7d3b39dea9e8f03593e3fb1ed
Roberto Arista <arista.rob@gmail.com>
2018-07-29 13:38:05 +0200

    drawing assistant moved to py3

---
+++ ttools/drawingAssistantComponents/sharedValues.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+PLUGIN_WIDTH = 230
+
+MARGIN_HOR = 10
+MARGIN_VER = 8
+
+NET_WIDTH = PLUGIN_WIDTH - MARGIN_HOR*2
+
+STEM_KEY = 'com.ttools.drawingAssistant_stems'
+DIAGONALS_KEY = 'com.ttools.drawingAssistant_diagonals'
+
+CURRENT_GLYPH_REPR = 'Current Glyph'
+CURRENT_FONT_REPR  = 'Current Font'
